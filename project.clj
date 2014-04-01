d351528d53eb7cf423a8441d31d3b444769a3087
Hugo Duncan <hugo@hugoduncan.org>
2014-04-01 13:09:54 -0400

    Updated project.clj, release notes and readme for 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/api-builder "0.1.0-SNAPSHOT"
+(defproject com.palletops/api-builder "0.1.0"
   :description "Write api functions with domain information."
   :url "http://github.com/palletops/api-builder"
   :license {:name "Eclipse Public License"
