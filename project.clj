4c8ae483cb3fd64cb3bb3c8a3f39ac6a71eef790
Hugo Duncan <hugo@hugoduncan.org>
2014-04-01 13:03:13 -0400

    Fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.palletops/api-builder "0.1.0-SNAPSHOT"
   :description "Write api functions with domain information."
-  :url "http://palletops.com/api-builder"
+  :url "http://github.com/palletops/api-builder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
