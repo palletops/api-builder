4b26d97892f35f373b31507d3f41ddb84a225aa5
Hugo Duncan <hugo@hugoduncan.org>
2014-05-01 08:57:19 -0400

    Remove unneeded configuration in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,1 @@
-{:dev {:plugins [[lein-pallet-release "RELEASE"]],
-       :pallet-release
-       {:url "https://pbors:${GH_TOKEN}@github.com/palletops/api-builder.git",
-        :branch "master"}},
- :no-checkouts {:checkout-deps-shares ^{:replace true} []},
- :release {:set-version {:updates [{:path "README.md",
-                                    :no-snapshot true}]}}}
+{:dev {:plugins [[lein-pallet-release "RELEASE"]]}}
