041a030c234267fb9f3633e961f56edec14a4283
Hugo Duncan <hugo@hugoduncan.org>
2014-08-27 16:52:05 -0400

    Remove logging from default api forms
    Adds the logged versions back into com.palletops.api-builder.api-logged.
    This is so the default versions can be used from clojurescript.

--- src/com/palletops/api_builder/api_logged.clj
+++ src/com/palletops/api_builder/api_logged.clj
@@ -1,20 +1,28 @@
-(ns com.palletops.api-builder.api
+(ns com.palletops.api-builder.api-logged
   "An API defn form that uses all stages"
   (:require
    [com.palletops.api-builder :refer [def-defn def-def def-defmulti]]
-   [com.palletops.api-builder.stage :refer :all]))
+   [com.palletops.api-builder.stage :refer :all]
+   [com.palletops.api-builder.stage.log :refer :all]))
+
 
 ;;; # API defn
 (def-defn defn-api
   [(validate-errors (constantly true))
    (validate-sig)
    (add-sig-doc)
+   (log-scope)
+   (log-entry)
+   (log-exit)
    (add-meta {:api true})])
 
 (def-defmulti defmulti-api
   [(validate-errors (constantly true))
    (validate-sig)
    (add-sig-doc)
+   (log-scope)
+   (log-entry)
+   (log-exit)
    (add-meta {:api true})])
 
 (def-def def-api
