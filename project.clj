57f78025caaac0faeb50434b578d9d9c82aebfe8
Hugo Duncan <hugo@hugoduncan.org>
2015-05-08 12:19:22 -0400

    Update schema and build tool dependencies
    Update to schema 0.4.2, and latest build tool dependencies.
    
    Closes #10

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.macro "0.1.2"]
-                 [prismatic/schema "0.2.6"]]
+                 [prismatic/schema "0.4.2"]]
   :prep-tasks ["cljx" "javac" "compile"]
   :test-paths ["test/clj" "target/generated/test/clj"]
   :aliases {"auto-test" ["do" "clean," "cljx," "cljsbuild" "auto" "test"]
